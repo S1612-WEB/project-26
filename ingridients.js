52f70407c92f9fb2cb749df4168acd070773c865
S1612-WEB <73536616+S1612-WEB@users.noreply.github.com>
2021-03-08 15:24:41 +0530

    Add files via upload

---
+++ ingridients.js
@@ -0,0 +1,19 @@
+Ingriedients 
+
+1. bowl
+2. sieve
+3. flour
+4. salt
+5. cocoa powder
+6. baking soda
+7. baking powder
+8. butter
+9. sugar
+10. eggs(2)
+11. vanilla extract
+12. red food colour
+13. cake tin
+14. oil
+15. butter paper
+16. creame cheese
+
